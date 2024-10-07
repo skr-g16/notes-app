0996b5ac098f11a9ccb4ef810ca8ac340da0aaa4
skr-g16 <fahmirty@gmail.com>
2024-10-07 12:09:02 +0700

    add divider-bar

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import addNotes from "./src/script/add-notes.js";
 import realTimeVal from "./src/script/realtime-val.js";
 import "./src/component/app-bar.js";
 import "./src/component/footer-app.js";
+import "./src/component/divider-bar.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   addNotes();
