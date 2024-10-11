ecee1d9f912996db82f98d7e59ee144d2bf7a247
skr-g16 <fahmirty@gmail.com>
2024-10-11 20:12:49 +0700

    update prettier setting

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-import "./style.css";
-import addNotes from "./src/script/add-notes.js";
-import realTimeVal from "./src/script/realtime-val.js";
-import "./src/component/app-bar.js";
-import "./src/component/footer-app.js";
-import "./src/component/divider-bar.js";
-import "./src/component/add-note-with-title.js";
+import './style.css';
+import addNotes from './src/script/add-notes.js';
+import realTimeVal from './src/script/realtime-val.js';
+import './src/component/app-bar.js';
+import './src/component/footer-app.js';
+import './src/component/divider-bar.js';
+import './src/component/add-note-with-title.js';
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   addNotes();
   realTimeVal();
-  document.dispatchEvent(new Event("render-notes"));
+  document.dispatchEvent(new Event('render-notes'));
 });
